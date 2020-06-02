c78088828ac8ef311378f68830a549d868caea47
Nishant Jain <najain@gmail.com>
2020-06-01 19:02:00 -0700

    Hyperparameter values chosen

--- graph.py
+++ graph.py
@@ -7,20 +7,36 @@ warnings.filterwarnings("ignore", module="networkx")
 
 # number of nodes
 N = 10000
+
+# expected number of neighbors per node
+# Chosen to be the median number of people met in a day by an individual:
+# https://www.researchgate.net/figure/Daily-average-number-of-contacts-per-person-in-age-group-j-The-average-number-of_fig2_228649013
+NUM_NEIGHBORS = 20
+
 # probability that two nodes are connected
-P_CONNECT = 0.005
+# Chosen so that the expected number of connections per node is NUM_NEIGHBORS
+P_CONNECT = 1/N * NUM_NEIGHBORS
 
 # seed ~1% of population as sick
 P_INIT_SICK = 0.01 
 
 # Probability someone is sick tests positive and is isolated
-TEST_POSITIVE = 0.8
-# probability of infection from unidentified sick node to healthy neighbor
-P_INFECT = 0.05 
-# probability of infection from isolated sick node to healthy neighbor
-P_INFECT_ISO = 0.01 
+# https://www.acc.org/latest-in-cardiology/journal-scans/2020/05/18/13/42/variation-in-false-negative-rate-of-reverse
+TEST_POSITIVE = 0.7
+
 # number of iterations/days
 NUM_ITERS = 10
+
+# Median Reproductive Number for COVID-19
+# https://wwwnc.cdc.gov/eid/article/26/7/20-0282_article
+R_0 = 5.7
+
+# probability of infection from unidentified sick node to healthy neighbor
+P_INFECT =  R_0/(NUM_NEIGHBORS * NUM_ITERS)
+
+# probability of infection from isolated sick node to healthy neighbor
+# We reduce it to 10% of P_INFECT
+P_INFECT_ISO = 0.1  * P_INFECT
 
 # probability that a neighbor is isolated in strategy c
 P_ISOLATE_NBR = 0.25
@@ -29,8 +45,8 @@ P_ISOLATE_NBR = 0.25
 SICK_NODES = set()
 
 # NODES THAT HAVE BEEN INFECTED, TESTED POSITIVE, AND ISOLATED
-# TP_NODES is a subset of SICK_NODES
-TP_NODES = set() 
+# ISOLATED_NODES is a subset of SICK_NODES
+ISOLATED_NODES = set() 
 
 def generate_graph():
 	G = nx.binomial_graph(N, P_CONNECT)
@@ -39,12 +55,12 @@ def generate_graph():
 def stepA(G):
 	for node in SICK_NODES:
 		if random.random() < TEST_POSITIVE:
-			TP_NODES.add(node)
+			ISOLATED_NODES.add(node)
 
 	sick_nodes = list(SICK_NODES.copy())
 	for node in sick_nodes:
 		for neighbor in G.neighbors(node):
-			if node in TP_NODES:
+			if node in ISOLATED_NODES:
 				if random.random() < P_INFECT_ISO:
 					SICK_NODES.add(neighbor)
 			else:
@@ -54,15 +70,15 @@ def stepA(G):
 def stepB(G):
 	for node in SICK_NODES:
 		if random.random() < TEST_POSITIVE:
-			TP_NODES.add(node)
+			ISOLATED_NODES.add(node)
 			# if someone tests positive, isolate all neighbors as well
 			for neighbor in G.neighbors(node):
-				TP_NODES.add(neighbor)
+				ISOLATED_NODES.add(neighbor)
 	
 	sick_nodes = list(SICK_NODES.copy())
 	for node in sick_nodes:
 		for neighbor in G.neighbors(node):
-			if node in TP_NODES:
+			if node in ISOLATED_NODES:
 				if random.random() < P_INFECT_ISO:
 					SICK_NODES.add(neighbor)
 			else:
@@ -72,16 +88,16 @@ def stepB(G):
 def stepB(G):
 	for node in SICK_NODES:
 		if random.random() < TEST_POSITIVE:
-			TP_NODES.add(node)
+			ISOLATED_NODES.add(node)
 			# if someone tests positive, isolate SOME neighbors as well
 			for neighbor in G.neighbors(node):
 				if random.random() < P_ISOLATE_NBR:
-					TP_NODES.add(neighbor)
+					ISOLATED_NODES.add(neighbor)
 	
 	sick_nodes = list(SICK_NODES.copy())
 	for node in sick_nodes:
 		for neighbor in G.neighbors(node):
-			if node in TP_NODES:
+			if node in ISOLATED_NODES:
 				if random.random() < P_INFECT_ISO:
 					SICK_NODES.add(neighbor)
 			else:
@@ -99,7 +115,7 @@ def draw(G):
 	color_map = ['green'] * G.number_of_nodes()
 	for n in SICK_NODES:
 		color_map[n] = "red"
-	for n in TP_NODES:
+	for n in ISOLATED_NODES:
 		color_map[n] = "orange"
 	nx.draw(G, node_color = color_map)
 	plt.show()
