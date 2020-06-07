97b859c76b51a4fc0746ccf490cc1251ebf66d0b
Nishant Jain <najain@gmail.com>
2020-06-06 19:18:27 -0700

    Cleanup simulator and add argument parsing and logging

--- graph.py
+++ graph.py
@@ -2,11 +2,16 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import sys
+import argparse
 
 import warnings
 warnings.filterwarnings("ignore", module="networkx")
 
-# number of nodes
+# testing and quarantine strategy. Default is unmitigated spread.
+# Options are A, B, C, D, E, F
+STRATEGY = "D"
+
+# number of nodes in social network
 N = 1000
 
 # expected number of neighbors per node
@@ -43,7 +48,10 @@ P_INFECT_ISO = 0.1  * P_INFECT
 P_ISOLATE_NBR = 0.25
 
 # number of tests you can perform in a time step.
-TIMESTEP_TEST_CAPACITY = 100
+TIMESTEP_TEST_CAPACITY = 10
+
+# Number of people to put in a group for pooled testing.
+POOL_SIZE = 8
 
 # ALL NODES THAT HAVE BEEN INFECTED
 SICK_NODES = set()
@@ -132,14 +140,40 @@ def stepD(G):
 
 # Everyone Isolated: models lockdown with some noisy transmission.
 def stepE(G):
-	for node in G:
-		ISOLATED_NODES.add(node)
-
 	sick_nodes = list(SICK_NODES.copy())
 	for node in sick_nodes:
 		for neighbor in G.neighbors(node):
 			if random.random() < P_INFECT_ISO:
 				SICK_NODES.add(neighbor)
+
+# Pooled testing with scatter groups.
+def stepF(G):
+	for test in range(TIMESTEP_TEST_CAPACITY):
+		quarWholeGroup = False
+		poolGroup = random.sample(list(G), POOL_SIZE)
+		for node in poolGroup:
+			if (node in SICK_NODES):
+				quarWholeGroup = True
+		if quarWholeGroup and random.random() < TEST_POSITIVE:
+			for node in poolGroup:
+				ISOLATED_NODES.add(node)
+				for neighbor in G.neighbors(node):
+					if random.random() < P_ISOLATE_NBR:
+						ISOLATED_NODES.add(neighbor)
+		if not quarWholeGroup:
+			for node in poolGroup:
+				if node in ISOLATED_NODES:
+					ISOLATED_NODES.remove(node)
+
+	sick_nodes = list(SICK_NODES.copy())
+	for node in sick_nodes:
+		for neighbor in G.neighbors(node):
+			if node in ISOLATED_NODES:
+				if random.random() < P_INFECT_ISO:
+					SICK_NODES.add(neighbor)
+			else:
+				if random.random() < P_INFECT:
+					SICK_NODES.add(neighbor)
 
 def draw(G):
 	color_map = ['green'] * G.number_of_nodes()
@@ -153,34 +187,112 @@ def draw(G):
 	nx.draw(G, node_color = color_map)
 	plt.show()
 
+# Calculate statistics and print out values.
+def spreadStatistics(G):
+	count = G.number_of_nodes()
+	healthyFree = 0
+	healthyIsolated = 0
+	sickFree = 0
+	sickIsolated = 0
+
+	for n in G:
+		if n in SICK_NODES:
+			if n in ISOLATED_NODES:
+				sickIsolated += 1
+			else:
+				sickFree += 1
+		else:
+			if n in ISOLATED_NODES:
+				healthyIsolated += 1
+			else:
+				healthyFree += 1
+	print("Number of healthy people free: " + str(healthyFree))
+	print("Number of infected people free: " + str(sickFree))
+	print("Number of infected people isolated: " + str(sickIsolated))
+	print("Number of healthy people isolated: " + str(healthyIsolated))
+
+
 def init_sick_nodes(G):
 	for node in G:
 		if random.random() < P_INIT_SICK:
 			SICK_NODES.add(node)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Please enter a valid strategy.")
+	parser = argparse.ArgumentParser(description='An agent based simulator for COVID-19 using a social network graph.')
+	parser.add_argument('--strategy', help='testing and quarantine strategy', required=False)
+	parser.add_argument('--population', type=int, help='number of nodes in the graph', required=False)
+	parser.add_argument('--test_capacity', type=int, help='number of tests that can be performed at each iteration', required=False)
+	parser.add_argument('--pool_size', type=int, help='number of nodes to pool in one test', required=False)
+	parser.add_argument('--iterations', type=int, help='number of time steps to iterate', required=False)
+	parser.add_argument('--r_0', type=float, help='viral reproductive number. Expected number of neighbors that a node should infect over the time duration.',
+	 required=False)
+	parser.add_argument('--test_positive', type=float, help='probability that someone who is sick tests positive',
+	 required=False)
+	parser.add_argument('--visualize', help='draws the final social network', action='store_true', required=False)
+	parser.add_argument('--p_init_sick', type=float, help='percentage of population to seed as sick at initialization. Decimal value betwen 0 and 1.',
+	 required=False)
+	args = parser.parse_args()
+
+	# Override defaults if flags given.
+	if args.population is not None:
+		N = args.population
+	if args.test_capacity is not None:
+		TIMESTEP_TEST_CAPACITY = args.test_capacity
+	if args.pool_size is not None:
+		POOL_SIZE = args.pool_size
+	if args.iterations is not None:
+		NUM_ITERS = args.iterations
+	if args.strategy is not None:
+		STRATEGY = args.strategy
+	if args.r_0 is not None:
+		R_0 = args.r_0
+	if args.test_positive is not None:
+		if args.test_positive >= 0 and args.test_positive <= 1:
+			TEST_POSITIVE = args.test_positive
+		else:
+			sys.exit("Error: test_positive = " + str(args.test_positive) + ". Please provide a test_positive probability between 0 and 1.")
+	if args.p_init_sick is not None:
+		if args.p_init_sick  >= 0 and args.p_init_sick  <= 1:
+			P_INIT_SICK = args.p_init_sick 
+		else:
+			sys.exit("Error: p_init_sick = " + str(args.p_init_sick ) + ". Please provide a p_init_sick probability between 0 and 1.")
+
+
+
+	# Function pointer that specifies what modification should occur at each step based on 
+	# the chosen strategy.
+	stepFunction = stepA
+	if STRATEGY == 'A':
+		stepFunction = stepA
+	elif STRATEGY == 'B':
+		stepFunction = stepB
+	elif STRATEGY == 'C':
+		stepFunction = stepC
+	elif STRATEGY == 'D':
+		stepFunction = stepD
+	elif STRATEGY == 'E':
+		stepFunction = stepE
+	elif STRATEGY == 'F':
+		stepFunction = stepF
+	else:
+		sys.exit("Incorrect strategy entered.")
 		exit()
 
 	G = generate_graph()
 	init_sick_nodes(G)
-	for i in range(NUM_ITERS):
-		print("Number of people infected at time " + str(i) + ": " + str(len(SICK_NODES)))
-		print("Number of people isolated at time " + str(i) + ": " + str(len(ISOLATED_NODES)))
 
-		if sys.argv[1] == 'A':
-			stepA(G)
-		elif sys.argv[1] == 'B':
-			stepB(G)
-		elif sys.argv[1] == 'C':
-			stepC(G)
-		elif sys.argv[1] == 'D':
-			stepD(G)
-		elif sys.argv[1] == 'E':
-			stepE(G)
-		else:
-			print("Incorrect strategy entered.")
-			exit()
-	draw(G)
+	# For certain scenarios, we need full lockdown at initialization.
+	if STRATEGY == "E" or STRATEGY == 'F':
+		for node in G:
+			ISOLATED_NODES.add(node)
+
+	for i in range(NUM_ITERS):
+		print("Time: " + str(i))
+		print("Time: " + str(i) + ": Number of people infected: " + str(len(SICK_NODES)))
+		print("Time: " + str(i) + ": Number of people isolated: " + str(len(ISOLATED_NODES)))
+		spreadStatistics(G)
+		stepFunction(G)
+
+	if args.visualize:
+		draw(G)
 	print("Number of people infected at end: " + str(len(SICK_NODES)))
